46b54a2404faff538f815c758a92abbc3746826d
Li Qi <166194652+asdadkji@users.noreply.github.com>
2025-08-21 22:18:27 +0800

    Delete src/stores directory

--- src/stores/category.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import type { Category } from '@/types/category'
-
-export const useAppStore = defineStore('app', () => {
-    const activeCategoryId = ref<number | null>(null)
-    const setActiveCategory = (id: number | null) => (activeCategoryId.value = id)
-    return { activeCategoryId, setActiveCategory }
-})
