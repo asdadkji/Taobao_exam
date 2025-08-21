c9199f48868e0588cfbcfbe5a749cc4babef1987
Li Qi <166194652+asdadkji@users.noreply.github.com>
2025-08-21 22:16:22 +0800

    Delete src/api directory

--- src/api/product/product.js
+++
@@ -1,7 +0,0 @@
-import service from '@/utils/axios.js'
-
-export const getCategories = () =>
-    service.get('/categories')
-
-export const getProducts = (params) =>
-    service.get('/products', { params })
--- src/api/register/register.js
+++
@@ -1,6 +0,0 @@
-import service from "@/utils/axios";
-
-export const register = async (payload) => {
-    const response = await service.post('/auth/register', payload);
-    return response.data;
-};
